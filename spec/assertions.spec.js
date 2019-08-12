616fd2a1afb6e2d1d880e8798d5c137df97635ed
shektor <s.hektor@gmail.com>
2019-08-12 14:37:26 +0100

    Apply javascript style lint updates

--- spec/assertions.spec.js
+++ spec/assertions.spec.js
@@ -1,32 +1,36 @@
-
 function runAllSpecs() {
+  var specMessage;
+  var specResults = [];
+  var pass = " PASS";
+  var fail = " FAIL";
 
-  var specMessage
-  var specResults = []
-  var pass = ' PASS'
-  var fail = ' FAIL'
+  specMessage = "Spec 01 - equal primitive type number: ";
+  assertEqualsShouldPassWith(specMessage, specResults, 1, 1);
 
-  specMessage = 'Spec 01 - equal primitive type number: '
-  assertEqualsShouldPassWith(specMessage, specResults, 1, 1)
+  specMessage = "Spec 02 - unequal primitive type number: ";
+  failMessage = ' Expected "1" found "2"';
+  assertEqualsShouldFailWith(specMessage, specResults, 1, 2, failMessage);
 
-  specMessage = 'Spec 02 - unequal primitive type number: '
-  failMessage = ' Expected "1" found "2"'
-  assertEqualsShouldFailWith(specMessage, specResults, 1, 2, failMessage)
+  specMessage = "Spec 03 - array type equal: ";
+  assertEqualsShouldPassWith(specMessage, specResults, [1, 1], [1, 1]);
 
-  specMessage = 'Spec 03 - array type equal: '
-  assertEqualsShouldPassWith(specMessage, specResults, [1, 1], [1, 1])
+  specMessage = "Spec 04 - array type unequal: ";
+  failMessage = " Expected type Array but found Object";
+  assertEqualsShouldFailWith(
+    specMessage,
+    specResults,
+    [1, 2],
+    { 1: 2 },
+    failMessage
+  );
 
-  specMessage = 'Spec 04 - array type unequal: '
-  failMessage = ' Expected type Array but found Object'
-  assertEqualsShouldFailWith(specMessage, specResults, [1, 2], { 1: 2 }, failMessage)
-
-  var specMessageEl = document.getElementById('specMessages');
+  var specMessageEl = document.getElementById("specMessages");
   var newSpecEl;
 
   for (var i = 0; i < specResults.length; i++) {
-    newSpecEl = document.createElement('li')
-    newSpecEl.innerHTML = specResults[i]
-    specMessageEl.appendChild(newSpecEl)
+    newSpecEl = document.createElement("li");
+    newSpecEl.innerHTML = specResults[i];
+    specMessageEl.appendChild(newSpecEl);
   }
 }
 
--- src/testRunner.js
+++ src/testRunner.js
@@ -1,4 +1,3 @@
-
 function runTest(message, assertionFailures, expected, actual) {
   try {
     assertEquals(message, expected, actual);
@@ -8,11 +7,10 @@ function runTest(message, assertionFailures, expected, actual) {
 }
 
 function runAll() {
-
   var complexObject1 = {
     propA: 1,
     propB: {
-      propA: [1, { propA: 'a', propB: 'b' }, 3],
+      propA: [1, { propA: "a", propB: "b" }, 3],
       propB: 1,
       propC: 2
     }
@@ -20,7 +18,7 @@ function runAll() {
   var complexObject1Copy = {
     propA: 1,
     propB: {
-      propA: [1, { propA: 'a', propB: 'b' }, 3],
+      propA: [1, { propA: "a", propB: "b" }, 3],
       propB: 1,
       propC: 2
     }
@@ -29,35 +27,35 @@ function runAll() {
     propA: 1,
     propB: {
       propB: 1,
-      propA: [1, { propA: 'a', propB: 'c' }, 3],
+      propA: [1, { propA: "a", propB: "c" }, 3],
       propC: 2
     }
   };
   var complexObject3 = {
     propA: 1,
     propB: {
-      propA: [1, { propA: 'a', propB: 'b' }, 3],
+      propA: [1, { propA: "a", propB: "b" }, 3],
       propB: 1
     }
   };
 
   var assertionFailures = [];
-  runTest('Test 01: ', assertionFailures, 'abc', 'abc');
-  runTest('Test 02: ', assertionFailures, 'abcdef', 'abc');
-  runTest('Test 03: ', assertionFailures, ['a'], {0: 'a'});
-  runTest('Test 04: ', assertionFailures, ['a', 'b'], ['a', 'b', 'c']);
-  runTest('Test 05: ', assertionFailures, ['a', 'b', 'c'], ['a', 'b', 'c']);
-  runTest('Test 06: ', assertionFailures, complexObject1, complexObject1Copy);
-  runTest('Test 07: ', assertionFailures, complexObject1, complexObject2);
-  runTest('Test 08: ', assertionFailures, complexObject1, complexObject3);
-  runTest('Test 09: ', assertionFailures, null, {});
+  runTest("Test 01: ", assertionFailures, "abc", "abc");
+  runTest("Test 02: ", assertionFailures, "abcdef", "abc");
+  runTest("Test 03: ", assertionFailures, ["a"], { 0: "a" });
+  runTest("Test 04: ", assertionFailures, ["a", "b"], ["a", "b", "c"]);
+  runTest("Test 05: ", assertionFailures, ["a", "b", "c"], ["a", "b", "c"]);
+  runTest("Test 06: ", assertionFailures, complexObject1, complexObject1Copy);
+  runTest("Test 07: ", assertionFailures, complexObject1, complexObject2);
+  runTest("Test 08: ", assertionFailures, complexObject1, complexObject3);
+  runTest("Test 09: ", assertionFailures, null, {});
 
-  var messagesEl = document.getElementById('messages');
+  var messagesEl = document.getElementById("messages");
   var newListEl;
   var i, ii;
 
   for (i = 0, ii = assertionFailures.length; i < ii; i++) {
-    newListEl = document.createElement('li');
+    newListEl = document.createElement("li");
     newListEl.innerHTML = assertionFailures[i];
     messagesEl.appendChild(newListEl);
   }

25453c7384d5e249fbe9cb79efde04e5266e8fa7
shektor <s.hektor@gmail.com>
2019-08-12 16:24:02 +0100

    Refactor array assertions
    The array assertions can be moved into their own functions can called 
    from within the try catch block of assertEquals.

--- src/assertions.js
+++ src/assertions.js
@@ -1,41 +1,37 @@
-
-function assertEquals (message, expected, actual) {
-  var expectedType = toStringCallTypeOf(expected)
-  var actualType = toStringCallTypeOf(actual)
+function assertEquals(message, expected, actual) {
+  var expectedType = toStringCallTypeOf(expected);
+  var actualType = toStringCallTypeOf(actual);
 
   try {
-    assertTypeEquality(message, expectedType, actualType)
+    assertTypeEquality(message, expectedType, actualType);
 
-    if (expectedType === 'Array') {
-      if (expected.length !== actual.length) {
-        throw { message: message + 'Expected Array length ' + expected.length + ' but found ' + actual.length }
-      }
-      for (var i = 0; i < expected.length; i++) {
-        if (expected[i] !== actual[i]) {
-          throw { message: message + 'Expected [' + i + '] "' + expected[i] + '" but found "' + actual[i] + '"'}
-        }
-      }
+    if (expectedType === "Array") {
+      assertArrayLength(message, expected, actual);
+      assertArrayEquality(message, expected, actual);
     } else {
-      assertPrimitiveEquality(message, expected, actual)
+      assertPrimitiveEquality(message, expected, actual);
     }
   } catch (failure) {
-    throw failure
+    throw failure;
   }
 
-  return true
+  return true;
 }
 
 function toStringCallTypeOf(operand) {
-  var type = Object.prototype.toString.call(operand)
-  type = type.slice(8,-1)
-  return type
+  var type = Object.prototype.toString.call(operand);
+  type = type.slice(8, -1);
+  return type;
 }
 
 function assertTypeEquality(message, expectedType, actualType) {
   if (expectedType === actualType) {
     return true;
   } else {
-    throw { message: message + ' Expected type ' + expectedType + ' but found ' + actualType }
+    throw {
+      message:
+        message + " Expected type " + expectedType + " but found " + actualType
+    };
   }
 }
 
@@ -43,6 +39,43 @@ function assertPrimitiveEquality(message, expected, actual) {
   if (expected === actual) {
     return true;
   } else {
-    throw { message: message + ' Expected "' + expected + '" but found "' + actual + '"' }
+    throw {
+      message:
+        message + ' Expected "' + expected + '" but found "' + actual + '"'
+    };
   }
+}
+
+function assertArrayLength(message, expected, actual) {
+  if (expected.length === actual.length) {
+    return true;
+  } else {
+    throw {
+      message:
+        message +
+        "Expected Array length " +
+        expected.length +
+        " but found " +
+        actual.length
+    };
+  }
+}
+
+function assertArrayEquality(message, expected, actual) {
+  for (var i = 0; i < expected.length; i++) {
+    if (expected[i] !== actual[i]) {
+      throw {
+        message:
+          message +
+          "Expected [" +
+          i +
+          '] "' +
+          expected[i] +
+          '" but found "' +
+          actual[i] +
+          '"'
+      };
+    }
+  }
+  return true;
 }

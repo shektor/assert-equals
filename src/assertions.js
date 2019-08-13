add9df2520897441226ea7fcf2c1693be2f6384c
shektor <s.hektor@gmail.com>
2019-08-13 15:19:11 +0100

    Update assertEquals to javascript ES6

--- src/assertions.js
+++ src/assertions.js
@@ -1,17 +1,17 @@
-function assertEquals(message, expected, actual) {
+const assertEquals = (message, expected, actual) => {
   try {
-    var stack = [];
+    const stack = [];
     assertDeepEquality(message, expected, actual, stack);
   } catch (failure) {
     throw failure;
   }
 
   return true;
-}
+};
 
-function assertDeepEquality(message, expected, actual, stack) {
-  var expectedType = toStringCallTypeOf(expected);
-  var actualType = toStringCallTypeOf(actual);
+const assertDeepEquality = (message, expected, actual, stack) => {
+  const expectedType = toStringCallTypeOf(expected);
+  const actualType = toStringCallTypeOf(actual);
 
   try {
     assertTypeEquality(message, expectedType, actualType);
@@ -27,74 +27,63 @@ function assertDeepEquality(message, expected, actual, stack) {
   }
 
   return true;
-}
+};
 
-function toStringCallTypeOf(operand) {
-  var type = Object.prototype.toString.call(operand);
+const toStringCallTypeOf = operand => {
+  let type = Object.prototype.toString.call(operand);
   type = type.slice(8, -1);
-  return type;
-}
 
-function assertTypeEquality(message, expectedType, actualType) {
+  return type;
+};
+
+const assertTypeEquality = (message, expectedType, actualType) => {
   if (expectedType === actualType) {
     return true;
   } else {
     throw {
-      message:
-        message + " Expected type " + expectedType + " but found " + actualType
+      message: `${message} Expected type ${expectedType} but found ${actualType}`
     };
   }
-}
+};
 
-function assertPrimitiveEquality(message, expected, actual, stack) {
+const assertPrimitiveEquality = (message, expected, actual, stack) => {
   if (expected === actual) {
     return true;
   } else {
     throw {
-      message:
-        message +
-        " Expected " +
-        stackDisplay(stack) +
-        '"' +
-        expected +
-        '" but found "' +
-        actual +
-        '"'
+      message: `${message} Expected ${stackDisplay(
+        stack
+      )}"${expected}" but found "${actual}"`
     };
   }
-}
+};
 
-function assertArrayLength(message, expected, actual) {
+const assertArrayLength = (message, expected, actual) => {
   if (expected.length === actual.length) {
     return true;
   } else {
     throw {
-      message:
-        message +
-        " Expected Array length " +
-        expected.length +
-        " but found " +
-        actual.length
+      message: `${message} Expected Array length ${expected.length} but found ${actual.length}`
     };
   }
-}
+};
 
-function assertArrayEquality(message, expected, actual, stack) {
-  for (var i = 0; i < expected.length; i++) {
-    stack.push("[" + i + "]");
-    if (assertDeepEquality(message, expected[i], actual[i], stack)) {
+const assertArrayEquality = (message, expected, actual, stack) => {
+  expected.forEach((element, index) => {
+    stack.push(`[${index}]`);
+    if (assertDeepEquality(message, expected[index], actual[index], stack)) {
       stack.pop();
     }
-  }
+  });
   return true;
-}
+};
 
-function stackDisplay(arrayStack) {
+const stackDisplay = arrayStack => {
   if (arrayStack.length > 0) {
-    arrayStack = arrayStack.join("") + " ";
+    arrayStack = `${arrayStack.join("")} `;
 
     return arrayStack;
   } else {
     return "";
   }
-}
+};

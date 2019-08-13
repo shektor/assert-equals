1c9714adf0e3ccad2a24e8fdd3db08e378ed7dd0
shektor <s.hektor@gmail.com>
2019-08-13 13:41:35 +0100

    Remove testRunner and update specHelper
    The testRunner is also performing the role of a spec file for assertions 
    while making it unclear as to what tests are passing.
    
    Remove testRunner and add functionality to specHelper so that failing 
    tests provide further details.

--- spec/specHelper.js
+++ spec/specHelper.js
@@ -9,9 +9,11 @@ function assertEqualsShouldPassWith(
       resultArray.push(specMessage + " PASS");
     } else {
       resultArray.push(specMessage + " FAIL");
+      resultArray.push('Expected "' + expected + '" and Actual "' + actual + '" did not return true' );
     }
   } catch (failure) {
     resultArray.push(specMessage + " FAIL");
+    resultArray.push('Expected "' + expected + '" and Actual "' + actual + '" threw exception <' + failure + '>');
   }
 }
 
@@ -25,11 +27,14 @@ function assertEqualsShouldFailWith(
   try {
     assertEquals(specMessage, expected, actual);
     resultArray.push(specMessage + " FAIL");
+    resultArray.push('Expected "' + expected + '" and Actual "' + actual + '" did not throw exception' );
   } catch (failure) {
-    if (failure.message === specMessage + " " + failMessage) {
+    expectedFailure = specMessage + " " + failMessage
+    if (failure.message === expectedFailure) {
       resultArray.push(specMessage + " PASS");
     } else {
       resultArray.push(specMessage + " FAIL");
+      resultArray.push('Expected exception <' + expectedFailure + '> but got <' + failure.message + '>');
     }
   }
 }

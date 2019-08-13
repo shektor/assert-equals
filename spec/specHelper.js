25f68cb9674a5e3666924b43e1b7a4117eb48fed
shektor <s.hektor@gmail.com>
2019-08-13 14:29:26 +0100

    Update specHelper to javascript ES6

--- spec/specHelper.js
+++ spec/specHelper.js
@@ -1,40 +1,51 @@
-function assertEqualsShouldPassWith(
+const assertEqualsShouldPassWith = (
   specMessage,
   resultArray,
   expected,
   actual
-) {
+) => {
   try {
     if (assertEquals(specMessage, expected, actual)) {
-      resultArray.push(specMessage + " PASS");
+      resultArray.push(`${specMessage} PASS`);
     } else {
-      resultArray.push(specMessage + " FAIL");
-      resultArray.push('Expected "' + expected + '" and Actual "' + actual + '" did not return true' );
+      resultArray.push(`${specMessage} FAIL`);
+      resultArray.push(
+        `Expected "${expected}" and Actual "${actual}" did not return true`
+      );
     }
   } catch (failure) {
-    resultArray.push(specMessage + " FAIL");
-    resultArray.push('Expected "' + expected + '" and Actual "' + actual + '" threw exception <' + failure + '>');
+    resultArray.push(`${specMessage} FAIL`);
+    resultArray.push(
+      `Expected "${expected}" and Actual "${actual}" threw exception <${JSON.stringify(
+        failure
+      )}>`
+    );
   }
-}
+};
 
-function assertEqualsShouldFailWith(
+const assertEqualsShouldFailWith = (
   specMessage,
   resultArray,
   expected,
   actual,
   failMessage
-) {
+) => {
   try {
     assertEquals(specMessage, expected, actual);
-    resultArray.push(specMessage + " FAIL");
-    resultArray.push('Expected "' + expected + '" and Actual "' + actual + '" did not throw exception' );
+    resultArray.push(`${specMessage} FAIL`);
+    resultArray.push(
+      `Expected "${expected}" and Actual "${actual}" did not throw exception`
+    );
   } catch (failure) {
-    expectedFailure = specMessage + " " + failMessage
+    expectedFailure = `${specMessage} ${failMessage}`;
+
     if (failure.message === expectedFailure) {
-      resultArray.push(specMessage + " PASS");
+      resultArray.push(`${specMessage} PASS`);
     } else {
-      resultArray.push(specMessage + " FAIL");
-      resultArray.push('Expected exception <' + expectedFailure + '> but got <' + failure.message + '>');
+      resultArray.push(`${specMessage} FAIL`);
+      resultArray.push(
+        `Expected exception <${expectedFailure}> but got <${failure.message}>`
+      );
     }
   }
-}
+};
